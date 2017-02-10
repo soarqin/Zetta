4d1346147b2711bbea1eccdc51fdbcc613c16342
Soar Qin <soarchin@gmail.com>
2017-02-10 15:26:33 +0800

    missing plugin

---
+++ plugins/Disgaea2/PluginMain.cpp
@@ -0,0 +1,273 @@
+#include "pch.cpp"
+
+#include "IPlugin.h"
+
+#include "Unit.h"
+
+#define IDH_F1 1900
+#define IDH_F2 1901
+#define IDH_F3 1902
+#define IDH_F4 1903
+#define IDH_F5 1904
+#define IDH_F6 1905
+#define IDH_F7 1906
+#define IDH_F8 1907
+#define IDH_F9 1908
+
+class CPluginPanel: public CWindowImpl<CPluginPanel, CStatic> {
+public:
+    void OnHotkey(int id, UINT, UINT);
+
+private:
+    BEGIN_MSG_MAP_EX(CPluginPanel)
+        MSG_WM_HOTKEY(OnHotkey);
+    END_MSG_MAP()
+};
+
+class PluginDisgaea2: public IPlugin {
+public:
+    virtual ~PluginDisgaea2() override {
+
+    }
+
+    virtual uint32_t Init(CAppModule* mod) override {
+        module_ = mod;
+        return 1;
+    }
+
+    virtual LPCWSTR GetName(uint32_t page) override {
+        return L"高级";
+    }
+
+    virtual bool Enable(IProcEdit* proc, void* tc) override {
+        if (proc->ClassName() != "Disgaea2 PC") return false;
+        if (proc->GetVersion() == "1.0.0.1") {
+            enemyCountOff_ = 0x242612;
+            enemyStartOff_ = 0x362628;
+            currentUnitOff_ = 0x3C1D80;
+            currentUnit_ = 0xC6628;
+            congressOff_ = 0xC6938;
+            congressCountOff_ = 0x3CE8F4;
+        } else return false;
+        proc_ = proc;
+        tabCtrl_ = (HWND)tc;
+        CFontHandle fnt;
+        fnt.CreatePointFont(200, _T("Arial"), 0);
+        CRect trc, rc;
+        tabCtrl_.GetItemRect(0, trc);
+        tabCtrl_.GetClientRect(rc);
+        rc.DeflateRect(8, trc.Height() + 8, 8, 8);
+        panel_.Create(tabCtrl_.m_hWnd, rc, 0, WS_CHILD | WS_BORDER);
+        CStatic lab[8];
+        lab[0].Create(panel_.m_hWnd, CRect(8, 18, 8 + rc.Width(), 48), _T("Ctrl+F1 我方HP/SP全满"), WS_VISIBLE | WS_CHILD);
+        lab[1].Create(panel_.m_hWnd, CRect(8, 58, 8 + rc.Width(), 88), _T("Ctrl+F2 所有敌人生命为0"), WS_VISIBLE | WS_CHILD);
+        lab[2].Create(panel_.m_hWnd, CRect(8, 98, 8 + rc.Width(), 128), _T("Ctrl+F3 当前目标HP/SP回满"), WS_VISIBLE | WS_CHILD);
+        lab[3].Create(panel_.m_hWnd, CRect(8, 138, 8 + rc.Width(), 168), _T("Ctrl+F5 当前目标HP为0"), WS_VISIBLE | WS_CHILD);
+        lab[4].Create(panel_.m_hWnd, CRect(8, 178, 8 + rc.Width(), 208), _T("Ctrl+F6 当前目标HP为1"), WS_VISIBLE | WS_CHILD);
+        lab[5].Create(panel_.m_hWnd, CRect(8, 218, 8 + rc.Width(), 248), _T("Ctrl+F7 议会议员好感为高"), WS_VISIBLE | WS_CHILD);
+        lab[6].Create(panel_.m_hWnd, CRect(8, 258, 8 + rc.Width(), 288), _T("Ctrl+F8 道具界下层为中转层"), WS_VISIBLE | WS_CHILD);
+        lab[7].Create(panel_.m_hWnd, CRect(8, 298, 8 + rc.Width(), 328), _T("Ctrl+F9 道具界99层"), WS_VISIBLE | WS_CHILD);
+        for (int i = 0; i < 8; ++i)
+            lab[i].SetFont(fnt);
+
+        for (int id = IDH_F1; id <= IDH_F9; ++id)
+			if (id != IDH_F4)
+				RegisterHotKey(panel_.m_hWnd, id, 0x4000 | MOD_CONTROL, VK_F1 + (id - IDH_F1));
+
+		return true;
+    }
+
+    virtual void Disable() override {
+        if (panel_.IsWindow()) {
+            panel_.DestroyWindow();
+            for (int id = IDH_F1; id <= IDH_F8; ++id)
+                UnregisterHotKey(panel_.m_hWnd, id);
+        }
+        panel_.m_hWnd = (HWND)0;
+        proc_ = nullptr;
+        tabCtrl_ = (HWND)0;
+        count_ = 0;
+        units_.clear();
+    }
+
+    virtual void Show(uint32_t page) override {
+        panel_.ShowWindow(SW_SHOW);
+    }
+
+    virtual void Hide(uint32_t page) override {
+        panel_.ShowWindow(SW_HIDE);
+    }
+
+    virtual void Tick(uint32_t page) override {
+        /*
+        uint16_t count;
+        proc_->Read(true, 0x45B10, &count, 2);
+        if (count != count_) {
+            count_ = count;
+            units_.resize(count);
+            if (count > 0)
+                proc_->Read(true, 0x1E98, &units_[0], sizeof(UnitInfo) * count);
+        }
+        uint32_t off;
+        proc_->Read(false, currentUnitOff_, &off, 4);
+        off += 12;
+        proc_->ReadRaw(off, &off, 4);
+        if (off != currentUnit_ && off >= 0x200000) {
+            currentUnit_ = off;
+            UnitInfo u;
+            proc_->ReadRaw(off, &u, sizeof(UnitInfo));
+        }
+        proc_->Read(true, enemyStartOff_, &off, 4);
+        if (off > 0) {
+            proc_->Read(true, enemyCountOff_, &count, 2);
+            std::vector<UnitInfo> enemies_;
+            enemies_.resize(count);
+            if (count > 0)
+                proc_->ReadRaw(off, &enemies_[0], sizeof(UnitInfo) * count);
+        }
+        */
+    }
+
+    bool OnF1() {
+        uint16_t count;
+        proc_->Read(true, 0xB4974, &count, 2);
+        if (count == 0) return false;
+        units_.resize(count);
+        proc_->Read(true, 0x7738, &units_[0], sizeof(UnitInfo) * count);
+        for (uint16_t i = 0; i < count; ++i) {
+            proc_->Write(true, 0x7738 + sizeof(UnitInfo) * i + offsetof(UnitInfo, HP), &units_[i].HPMax, 4);
+            proc_->Write(true, 0x7738 + sizeof(UnitInfo) * i + offsetof(UnitInfo, SP), &units_[i].SPMax, 4);
+        }
+        return true;
+    }
+
+    bool OnF2() {
+        uint16_t count;
+        proc_->Read(false, enemyCountOff_, &count, 2);
+        if (count == 0) return false;
+        uint32_t off;
+        proc_->Read(false, enemyStartOff_, &off, 4);
+        if (off == 0) return false;
+        uint32_t hp = 0;
+        for (uint16_t i = 0; i < 64; ++i)
+            proc_->WriteRaw(off + sizeof(UnitInfo) * i + offsetof(UnitInfo, HP), &hp, 4);
+        return true;
+    }
+
+    bool OnF3() {
+        uint32_t off;
+        proc_->Read(true, currentUnitOff_, &off, 4);
+        proc_->Read(true, currentUnit_ + off * 0x388, &off, 4);
+        if (off < 0x200000) return false;
+        UnitInfo u;
+        proc_->ReadRaw(off, &u, sizeof(UnitInfo));
+        proc_->WriteRaw(off + offsetof(UnitInfo, HP), &u.HPMax, 4);
+        proc_->WriteRaw(off + offsetof(UnitInfo, SP), &u.SPMax, 4);
+        return true;
+    }
+
+    bool OnF5() {
+        uint32_t off;
+        proc_->Read(true, currentUnitOff_, &off, 4);
+        proc_->Read(true, currentUnit_ + off * 0x388, &off, 4);
+        if (off < 0x200000) return false;
+        uint32_t hp = 0;
+        proc_->WriteRaw(off + offsetof(UnitInfo, HP), &hp, 4);
+        return true;
+    }
+
+    bool OnF6() {
+        uint32_t off;
+        proc_->Read(true, currentUnitOff_, &off, 4);
+        proc_->Read(true, currentUnit_ + off * 0x388, &off, 4);
+        if (off < 0x200000) return false;
+        uint32_t hp = 1;
+        proc_->WriteRaw(off + offsetof(UnitInfo, HP), &hp, 4);
+        return true;
+    }
+
+    bool OnF7() {
+        uint8_t count;
+        int16_t n = 120;
+        proc_->Read(true, congressCountOff_, &count, 1);
+        for (uint8_t i = 0; i < count; ++i) {
+            proc_->Write(true, congressOff_ + 0x388 * i, &n, 2);
+        }
+        return true;
+    }
+
+    bool OnF8() {
+        uint16_t count;
+        proc_->Read(false, enemyCountOff_, &count, 2);
+        if (count == 0) return false;
+        uint8_t level;
+        proc_->Read(true, 0xB49AC, &level, 1);
+        if (level >= 99) return false;
+        if ((level + 1) % 10 == 0) level += 9;
+        else level = level / 10 * 10 + 8;
+        proc_->Write(true, 0xB49AC, &level, 1);
+        return true;
+    }
+
+    bool OnF9() {
+        uint16_t count;
+        proc_->Read(false, enemyCountOff_, &count, 2);
+        if (count == 0) return false;
+        uint8_t level = 98;
+        proc_->Write(true, 0xB49AC, &level, 1);
+        return true;
+    }
+
+private:
+    CAppModule* module_ = nullptr;
+    IProcEdit* proc_ = nullptr;
+    CTabCtrl tabCtrl_;
+    CPluginPanel panel_;
+    uint16_t count_ = 0;
+    std::vector<UnitInfo> units_;
+    uintptr_t enemyStartOff_ = 0;
+    uintptr_t enemyCountOff_ = 0;
+    uintptr_t currentUnitOff_ = 0;
+    uintptr_t currentUnit_ = 0;
+    uintptr_t congressOff_ = 0;
+    uintptr_t congressCountOff_ = 0;
+};
+
+static PluginDisgaea2 gPlugin;
+
+void CPluginPanel::OnHotkey(int id, UINT, UINT) {
+    switch (id) {
+    case IDH_F1:
+        if (!gPlugin.OnF1()) return;
+        break;
+    case IDH_F2:
+        if (!gPlugin.OnF2()) return;
+        break;
+    case IDH_F3:
+        if (!gPlugin.OnF3()) return;
+        break;
+    case IDH_F5:
+        if (!gPlugin.OnF5()) return;
+        break;
+    case IDH_F6:
+        if (!gPlugin.OnF6()) return;
+        break;
+    case IDH_F7:
+        if (!gPlugin.OnF7()) return;
+        break;
+    case IDH_F8:
+        if (!gPlugin.OnF8()) return;
+        break;
+	case IDH_F9:
+		if (!gPlugin.OnF9()) return;
+		break;
+	default:
+        return;
+    }
+    Beep(300, 300);
+}
+
+IPlugin* WINAPI GetPlugin() {
+#pragma comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
+    return &gPlugin;
+}
---
+++ plugins/Disgaea2/Unit.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#pragma pack(push, 1)
+struct UnitInfo {
+	uint64_t exp;         // +8
+	uint8_t equip[0x600]; // +0x608
+	uint8_t name[0x40];   // +0x648
+	uint8_t job[0x40];    // +0x688
+	uint8_t unk1[0x48];   // +0x6D0
+	uint16_t sres[5];     // +0x6DA
+	uint8_t unk2[0x72];   // +0x74C
+	uint32_t skillExp[0x70]; // +0x90C
+	uint16_t skillId[0x70]; // +0x9EC
+	uint8_t skillLevel[0x70]; // +0xA5C
+	uint16_t skillIndex[0x70]; // +0xB3C
+	uint16_t skillId2[0x70]; // +0xC1C
+	uint32_t unk3; // +0xC20
+	uint32_t HP;
+	uint32_t SP;
+	uint32_t HPMax;
+	uint32_t SPMax;
+	uint32_t ATK;
+	uint32_t DEF;
+	uint32_t INT;
+	uint32_t SPD;
+	uint32_t HIT;
+	uint32_t RES;
+	uint32_t HPBase;
+	uint32_t SPBase;
+	uint32_t ATKBase;
+	uint32_t DEFBase;
+	uint32_t INTBase;
+	uint32_t SPDBase;
+	uint32_t HITBase;
+	uint32_t RESBase;
+	uint32_t FistExp;
+	uint32_t SwordExp;
+	uint32_t SpearExp;
+	uint32_t BowExp;
+	uint32_t GunExp;
+	uint32_t AxeExp;
+	uint32_t StaffExp;
+	uint32_t unk4;
+	uint32_t Mana; // +0xC8C
+	uint8_t unk5[0x18]; // +0xCA4
+	uint8_t FistLevel;
+	uint8_t SwordLevel;
+	uint8_t SpearLevel;
+	uint8_t BowLevel;
+	uint8_t GunLevel;
+	uint8_t AxeLevel;
+	uint8_t StaffLevel;
+	uint8_t unk6[1]; // +0xCAC
+	uint8_t FistClass;
+	uint8_t SwordClass;
+	uint8_t SpearClass;
+	uint8_t BowClass;
+	uint8_t GunClass;
+	uint8_t AxeClass;
+	uint8_t StaffClass;
+	uint8_t unk7[0x09];
+    uint8_t HPGrowth;
+	uint8_t SPGrowth;
+	uint8_t ATKGrowth;
+	uint8_t DEFGrowth;
+	uint8_t INTGrowth;
+	uint8_t SPDGrowth;
+	uint8_t HITGrowth;
+	uint8_t RESGrowth;
+	uint8_t unk8[0x08]; // +0xCCC
+	uint32_t lvl; // +0xCD0
+	uint8_t unk9[0x14]; // +0xCE4
+	uint8_t JmMx;
+	uint8_t Jm;
+	uint8_t MvMx;
+	uint8_t Mv;
+	uint8_t CountMx;
+	uint8_t Counter; // +0xCEA
+	uint8_t unk10; // +0xCEB
+	uint8_t skillCount; //0xCEC
+	uint8_t unk11[0x214];
+};
+#pragma pack(pop)
